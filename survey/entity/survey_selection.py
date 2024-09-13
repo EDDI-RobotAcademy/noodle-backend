e59c13eb1bdb956ea29195f8c7a9c188557fb902
Meue-L <meue7785@gmail.com>
2024-09-13 16:09:35 +0900

    feat: [MinJaeLee/Meue] 질문과 선택지에 대한 텍스트를 담을 수 있는 필드 추가 [NBD-4]

--- survey/entity/survey_selection.py
+++ survey/entity/survey_selection.py
@@ -7,6 +7,7 @@ class SurveySelection(models.Model):
     id = models.AutoField(primary_key=True)
     SurveyQuestionID = models.ForeignKey(SurveyQuestion, on_delete=models.CASCADE)
     SurveySelectionNumber = models.IntegerField(default=0)
+    SurveySelectionSentence = models.CharField(max_length=100, default=None)
 
     class Meta:
         db_table = "survey_selection"
