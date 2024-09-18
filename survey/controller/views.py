004097951fca7f51338fdfef78ca3879f021a887
Meue-L <meue7785@gmail.com>
2024-09-19 08:53:52 +0900

    feat: [MinJaeLee/Meue] 설문조사에 대한 내용 누락 검사 로직 추가 [NBD-2, NBD-5]

--- survey/controller/views.py
+++ survey/controller/views.py
@@ -15,10 +15,13 @@ class SurveyView(viewsets.ViewSet):
             surveyQuestionSentence = request.data.get("surveyQuestionSentence")
             surveySelectionList = request.data.get("surveySelectionList")
 
+            if not surveyID or not surveyQuestionNumber or not surveyQuestionSentence or not surveySelectionList:
+                return Response({'response': 'There is no content'}, status=status.HTTP_204_NO_CONTENT)
+
             self.surveyService.registerNewSurvey(surveyID, surveyQuestionNumber, surveyQuestionSentence,
                                                  surveySelectionList)
 
-            return Response({'response': True}, status=status.HTTP_200_OK)
+            return Response({'response': True}, status=status.HTTP_201_CREATED)
         except Exception as e:
             print("error occured while registering survey :", e)
             return Response({'response': e}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
@@ -29,6 +32,9 @@ class SurveyView(viewsets.ViewSet):
             surveyNumber = request.data.get("surveyNumber")
             surveyQuestionNumber = request.data.get("surveyQuestionNumber")
             surveySelectionNumber = request.data.get("surveySelectionNumber")
+
+            if not surveyNumber or not surveyQuestionNumber or not surveySelectionNumber:
+                return Response({'response': 'There is no content'}, status.HTTP_204_NO_CONTENT)
 
             self.surveyService.saveSurveyAnswer(surveyNumber, surveyQuestionNumber, surveySelectionNumber)
 
