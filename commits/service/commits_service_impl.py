5c32ea18aae4a5c44751cad7c5b4cf2b0cf6b474
Jaehyuk Han <jh.jaehyuk213@gmail.com>
2024-09-24 17:41:13 +0900

    add: [한재혁] lastPageNumber를 이용해서 저장소의 커밋 내역을 전부 가져올 수 있음 [NDB-16]

--- commits/service/commits_service_impl.py
+++ commits/service/commits_service_impl.py
@@ -20,9 +20,17 @@ class CommitsServiceImpl(CommitsService):
         return cls.__instance
 
     def list(self, username, accessToken, reponame, branchname, pageNumber):
-        commits = self.__commitsRepository.getCommitsOfPage(username, accessToken, reponame, branchname, pageNumber)
         commitList = []
+
+        commits, lastPageNumber = self.__commitsRepository.getCommitsOfPage(username, accessToken, reponame, branchname, pageNumber)
+
         for commit in commits:
             commitList.append(commit['commit']['message'])
+
+        for page in range(2, lastPageNumber + 1):
+            commits = self.__commitsRepository.getCommitsOfPage(username, accessToken, reponame, branchname, page)
+
+            for commit in commits:
+                commitList.append(commit['commit']['message'])
 
         return commitList
