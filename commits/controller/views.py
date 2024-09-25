7f4fb527fa9ed0e9293d7477776711a344e69edc
Jaehyuk Han <jh.jaehyuk213@gmail.com>
2024-09-25 19:07:26 +0900

    feat: [한재혁] 사용자가 요청한 저장소의 특정 브랜치 커밋 내역들을 데이터베이스에 저장합니다. [NBD-21]

--- commits/controller/views.py
+++ commits/controller/views.py
@@ -3,17 +3,21 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 
 from commits.service.commits_service_impl import CommitsServiceImpl
+from github_oauth.service.redis_service_impl import RedisServiceImpl
 
 
 class CommitsView(viewsets.ViewSet):
     commitsService = CommitsServiceImpl.getInstance()
+    redisService = RedisServiceImpl.getInstance()
 
-    def list(self, request):
-        username = request.data['username']
+    def save(self, request):
+        userToken = request.data['userToken']
         reponame = request.data['reponame']
-        accessToken = request.data['access_token']
         branchname = request.data['branchname']
 
-        commitList = self.commitsService.list(username, accessToken, reponame, branchname)
+        accountId = self.redisService.getValueByKey(userToken)
+        accessToken = self.redisService.getValueByKey(accountId)
 
-        return Response({"commit_list": commitList}, status=status.HTTP_200_OK)
+        self.commitsService.save(accountId, accessToken, reponame, branchname)
+
+        return Response(status=status.HTTP_200_OK)
--- commits/repository/commits_repository.py
+++ commits/repository/commits_repository.py
@@ -3,5 +3,5 @@ from abc import ABC, abstractmethod
 
 class CommitsRepository(ABC):
     @abstractmethod
-    def getCommitsOfPage(self, username, accessToken, reponame, branchname, pageNumber):
+    def saveCommits(self, account, accessToken, repo, branch):
         pass
