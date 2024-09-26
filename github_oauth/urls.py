8625c816343f31e28f8a3b5598d071380f894e3a
Meue-L <meue7785@gmail.com>
2024-09-26 14:31:46 +0900

    refactor: [MinJaeLee/Meue] 보안 정책에 따른 2개 함수 통합 [NBD-11]

--- github_oauth/urls.py
+++ github_oauth/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     path('', include(router.urls)),
     path('github', OauthView.as_view({'get': 'githubOauthURI'}), name='get-github-oauth-uri'),
     path('github/access-token', OauthView.as_view({'post': 'githubAccessTokenURL'}), name="get-github-access-token-url"),
-    path('github/user-info', OauthView.as_view({'post': 'githubUserInfoURL'}), name="get-github-user-info-url"),
 ]
