fc76e85815a3bf402b8dea685c43bbc168d162e1
jimin9703 <moses0322@naver.com>
2024-10-07 09:04:09 +0900

    fix:[김지민] 반환값 수정 [NBD-33]

--- backlog/controller/views.py
+++ backlog/controller/views.py
@@ -15,6 +15,6 @@ class BacklogView(viewsets.ViewSet):
         if not title:
             return Response({"error": "제목이 필요합니다"}, status=status.HTTP_400_BAD_REQUEST)
 
-        createdBacklog = self.backlogService.createBacklog(title)
+        createdBacklog = self.backlogService.createBacklog(title).title
 
         return Response(createdBacklog, status=status.HTTP_200_OK)
