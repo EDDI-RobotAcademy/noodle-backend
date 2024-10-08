199ebd5fae243754ffbd47c00d29c0764f8f0b9a
Meue-L <meue7785@gmail.com>
2024-10-08 10:37:16 +0900

    feat: [MinJaeLee/Meue] 자유 형식의 리뷰를 DB에 등록할 수 있습니다 [NBD-47]

--- review/urls.py
+++ review/urls.py
@@ -9,7 +9,7 @@ router.register(r'review', ReviewView)
 urlpatterns = [
     path('', include(router.urls)),
     path('list', ReviewView.as_view({'post': 'reviewList'}), name='review-list'),
-    path('register', ReviewView.as_view({'post': 'create'}), name='review-register'),
+    path('register/writingReview', ReviewView.as_view({'post': 'registerNewWritingReview'}), name='writing-review-register'),
     path('read/<int:pk>', ReviewView.as_view({'get': 'read'}), name='review-read'),
     path('entire-count', ReviewView.as_view({'post': 'entireReviewListCount'}), name='review-list-entire-count'),
 ]
