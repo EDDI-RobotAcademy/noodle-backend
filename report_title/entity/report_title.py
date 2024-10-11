979a67da3631e599c38d43decbff96075150fbc1
Jaehyuk Han <jh.jaehyuk213@gmail.com>
2024-10-11 10:15:05 +0900

    fix: [한재혁] related_name 오류로 인해 migrate 안되는 문제 해결 [NBD-52]

--- report_title/entity/report_title.py
+++ report_title/entity/report_title.py
@@ -6,7 +6,7 @@ from report.entity.report import ResultReport
 class ResultReportTitle(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=128)
-    report = models.ForeignKey(ResultReport, on_delete=models.CASCADE, related_name="report-title")
+    report = models.ForeignKey(ResultReport, on_delete=models.CASCADE, related_name="title")
 
     def __str__(self):
         return f"Result Report Id: {self.report.id} -> Result Report Title: {self.title}"
