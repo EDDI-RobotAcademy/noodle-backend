0aae8eb905a48a802e0de95f4dfbd3d8bbc22706
Jaehyuk Han <jh.jaehyuk213@gmail.com>
2024-10-17 11:22:34 +0900

    feat: [한재혁] 결과 보고서로 해당 보고서의 주요 기능 번호를 찾을 수 있습니다. [NBD-60]

--- report_feature/repository/report_feature_repository.py
+++ report_feature/repository/report_feature_repository.py
@@ -3,5 +3,9 @@ from abc import abstractmethod, ABC
 
 class ResultReportFeatureRepository(ABC):
     @abstractmethod
+    def getResultReportFeatureById(self, id):
+        pass
+
+    @abstractmethod
     def createResultReportFeature(self, report):
         pass
