da93ec79e5f6a39e3e382e36b5ad6f27abfd8b27
Jaehyuk Han <jh.jaehyuk213@gmail.com>
2024-10-23 17:14:17 +0900

    fix: [한재혁] backlog 도메인에서 다른 backlog 항목들도 한번에 생성되도록 수정 [NBD-33]

--- backlog/controller/views.py
+++ backlog/controller/views.py
@@ -10,11 +10,13 @@ class BacklogView(viewsets.ViewSet):
 
     def createBacklog(self, request):
         data = request.data
-        title = data.get('title')
+        userToken = data.get('userToken')
+        result = data.get('aiResult')
+        backlogList = result['backlog']
 
-        if not title:
+        if not backlogList:
             return Response({"error": "제목이 필요합니다"}, status=status.HTTP_400_BAD_REQUEST)
 
-        createdBacklog = self.backlogService.createBacklog(title).title
+        createdBacklog = self.backlogService.createBacklog(backlogList).title
 
         return Response(createdBacklog, status=status.HTTP_200_OK)
