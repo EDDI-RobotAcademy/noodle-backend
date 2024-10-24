33f9108b96c95a80a1bba409fddb606d9c7e6cd5
Jaehyuk Han <jh.jaehyuk213@gmail.com>
2024-10-24 11:40:42 +0900

    fix: [한재혁] 결과 보고서의 기술 스택들을 bulk_create 하도록 수정 [NBD-56]

--- report_skill/repository/report_skill_repository.py
+++ report_skill/repository/report_skill_repository.py
@@ -3,5 +3,5 @@ from abc import abstractmethod, ABC
 
 class ResultReportSkillRepository(ABC):
     @abstractmethod
-    def create(self, skill, skillset):
+    def create(self, skillList, skillset):
         pass
