5ab4e19488b1ae4e181382a3496c3fd52b1b2c8f
Jaehyuk Han <jh.jaehyuk213@gmail.com>
2024-10-24 11:42:19 +0900

    fix: [한재혁] 결과 보고서의 팀 번호를 저장할 때 결과 보고서를 외래키로 참조할 수 있도록 수정 [NBD-53]

--- report_team/service/report_team_service_impl.py
+++ report_team/service/report_team_service_impl.py
@@ -1,3 +1,4 @@
+from report.repository.report_repository_impl import ResultReportRepositoryImpl
 from report_team.repository.result_team_repository_impl import ResultReportTeamRepositoryImpl
 from report_team.service.report_team_service import ResultReportTeamService
 
@@ -9,6 +10,7 @@ class ResultReportTeamServiceImpl(ResultReportTeamService):
         if cls.__instance is None:
             cls.__instance = super().__new__(cls)
             cls.__instance.__resultReportTeamRepository = ResultReportTeamRepositoryImpl.getInstance()
+            cls.__instance.__resultReportRepository = ResultReportRepositoryImpl.getInstance()
 
         return cls.__instance
 
@@ -20,4 +22,5 @@ class ResultReportTeamServiceImpl(ResultReportTeamService):
         return cls.__instance
 
     def createResultReportTeamService(self, resultReportId):
-        self.__resultReportTeamRepository.create(resultReportId)
+        report = self.__resultReportRepository.getReportById(resultReportId)
+        self.__resultReportTeamRepository.create(report)
