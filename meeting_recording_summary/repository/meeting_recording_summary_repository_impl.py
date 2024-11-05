55f736534a10e0b975c5c3dca985c2ded6f8869c
Jaehyuk Han <jh.jaehyuk213@gmail.com>
2024-11-05 16:26:18 +0900

    feat: [한재혁] 사용자가 저장 요청한 회의록 요약본을 데이터베이스에 저장합니다. [NBD-109]

---
+++ meeting_recording_summary/repository/meeting_recording_summary_repository_impl.py
@@ -0,0 +1,26 @@
+from meeting_recording_summary.entity.meeting_recording_summary import MeetingRecordingSummary
+from meeting_recording_summary.repository.meeting_recording_summary_repository import MeetingRecordingSummaryRepository
+
+
+class MeetingRecordingSummaryRepositoryImpl(MeetingRecordingSummaryRepository):
+    __instance = None
+
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+
+        return cls.__instance
+
+    @classmethod
+    def getInstance(cls):
+        if cls.__instance is None:
+            cls.__instance = cls()
+
+        return cls.__instance
+
+    def createSummary(self, username, title, content):
+        meetingRecordingSummary = MeetingRecordingSummary.objects.create(title=title, writer=username, content=content)
+
+        return meetingRecordingSummary
+
+
---
+++ meeting_recording_summary/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from meeting_recording_summary.controller.views import MeetingRecordingSummaryView
+
+router = DefaultRouter()
+router.register(r"meeting_recording_summary", MeetingRecordingSummaryView, basename="meeting_recording_summary")
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path("create", MeetingRecordingSummaryView.as_view({"post": "create"}), name="create-meeting-recording-summary"),
+]
